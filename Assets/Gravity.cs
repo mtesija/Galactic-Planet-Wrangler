0eeb794b30fc3177e61cf452e3533b5bfcc40b5a
Michael Tesija <mtesija@umich.edu>
2014-08-23 18:13:53 -0400

    TAKE 2

---
+++ Assets/Gravity.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour
+{
+	float G = 10;
+
+	void Update()
+	{
+		GameObject[] Objects = GameObject.FindGameObjectsWithTag("Gravity") as GameObject[];
+		foreach(GameObject obj in Objects)
+		{
+			float distance = Vector2.Distance(obj.transform.position, this.transform.position);
+
+			if(distance == 0)
+			{
+				continue;
+			}
+
+			Vector2 direction = obj.transform.position - this.transform.position;
+			direction.Normalize();
+
+			direction *= G * obj.rigidbody2D.mass / (distance * distance);
+
+			this.rigidbody2D.AddForce(direction);
+		}
+	}
+}
