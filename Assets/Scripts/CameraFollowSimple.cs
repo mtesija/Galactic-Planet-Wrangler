43ea1a8b737d354d6f878623a38bade87bab8ef0
atesija <antesija@gmail.com>
2014-08-24 17:09:43 -0400

    Camera zoom

---
+++ Assets/Scripts/CameraFollowSimple.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowSimple : MonoBehaviour {
+
+	public GameObject objectToFollow;
+	public float smoothSpeed;
+	public float lockTime;
+	float timer;
+	bool lockout;
+	
+	// Use this for initialization
+	void Start () {
+		if(!objectToFollow)
+		{
+			Debug.Log("Camera has no object to follow");
+			return;
+		}
+		timer = 0.0f;
+		lockout = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!objectToFollow)
+		{
+			Debug.Log("Camera has no object to follow");
+			return;
+		}
+		timer += Time.deltaTime;
+		if(timer >= lockTime && !lockout)
+		{
+			smoothSpeed += 1.0f * Time.deltaTime;
+			if(smoothSpeed >= 50.0f)
+				lockout = true;
+		}
+
+		Vector3 v = Vector3.Lerp(transform.position, objectToFollow.transform.position, smoothSpeed * Time.deltaTime);
+		v.z = -10;
+		transform.position = v;
+	}
+}
---
+++ Assets/Scripts/InitialZoom.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitialZoom : MonoBehaviour {
+
+	public Camera c;
+	public float zoomSpeed;
+	public float zoomTo;
+	public float zoomTime;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer >= zoomTime)
+		{
+
+			float zoom = Input.GetAxis("Mouse ScrollWheel");
+			c.orthographicSize += zoom * -1.0f;
+			if(c.orthographicSize >= 30.0f)
+			{
+				c.orthographicSize = 30.0f;
+			}
+			if(c.orthographicSize <= 3.0f)
+			{
+				c.orthographicSize = 3.0f;
+			}
+
+			return;
+		}
+		c.orthographicSize = Mathf.Lerp(c.orthographicSize, zoomTo, zoomSpeed * Time.deltaTime);
+	}
+}
