99f3dbb15349b373697da41374fa1ff1251202c5
Michael Tesija <mtesija@umich.edu>
2014-08-24 17:13:14 -0400

    redfshznkl [asdrzlk; hzrfd

---
+++ Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSound : MonoBehaviour
+{
+	public AudioClip yeehaw1;
+	public AudioClip yeehaw2;
+
+	float cooldown = 0f;
+
+	void Update()
+	{
+		if(cooldown > 0)
+		{
+			cooldown -= Time.deltaTime;
+		}
+
+		if(Input.GetMouseButtonDown(1) && cooldown <= 0)
+		{
+			if(Random.Range(0, 2) == 0)
+			{
+				this.audio.clip = yeehaw1;
+				this.audio.Play();
+			}
+			else
+			{
+				this.audio.clip = yeehaw2;
+				this.audio.Play();
+			}
+
+			cooldown = 1.5f;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if(cooldown > 0)
+		{
+			return;
+		}
+
+		if(Random.Range(0, 2) == 0)
+		{
+			this.audio.clip = yeehaw1;
+			this.audio.Play();
+		}
+		else
+		{
+			this.audio.clip = yeehaw2;
+			this.audio.Play();
+		}
+		
+		cooldown = 1.5f;
+	}
+}
