95a2fc78d4b334e98149261e03d10374998b85e8
atesija <antesija@gmail.com>
2014-08-24 12:09:26 -0400

    Intro

---
+++ Assets/Scripts/FadeOverTime.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeOverTime : MonoBehaviour {
+
+	public float fadeTime;
+	float totalTime;
+	SpriteRenderer spriteRenderer;
+	bool stopTrying;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = (SpriteRenderer)transform.GetComponent("SpriteRenderer");
+		totalTime = 0.0f;
+		stopTrying = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(stopTrying)
+		{
+			return;
+		}
+		totalTime += Time.deltaTime;
+		Color color = spriteRenderer.color;
+		color.a = Mathf.Lerp(1f, 0f, totalTime / fadeTime);
+		spriteRenderer.color = color;
+		if(color.a <= 0)
+		{
+			stopTrying = true;
+	
+		}
+	}
+}
