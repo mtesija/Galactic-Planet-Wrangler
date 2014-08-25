5d91b9f2c1e32fb528ad9f408a64a26eb12e1773
Michael Tesija <mtesija@umich.edu>
2014-08-25 04:40:33 -0400

    Still need a background for the score text

--- Assets/Scripts/Scorekeeper.cs
+++ Assets/Scripts/Scorekeeper.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class Scorekeeper : MonoBehaviour
 {
 	bool Endgame = false;
+	bool displayGUI = false;
 	float EndTimer = 3f;
 
 	float Score = 0;
 	float NumberPlanets = 0;
 	float NumberAsteroids = 0;
-	float TotalMass = 0;
+	float TotalAsteroidMass = 0;
+	float TotalPlanetMass = 0;
 	float Speed = 0;
 
 	public AudioClip hit1;
@@ -19,6 +21,11 @@ public class Scorekeeper : MonoBehaviour
 
 	void Update()
 	{
+		if(cooldown > 0)
+		{
+			cooldown -= Time.deltaTime;
+		}
+
 		if(Endgame)
 		{
 			if(EndTimer > 0)
@@ -27,26 +34,27 @@ public class Scorekeeper : MonoBehaviour
 			}
 			else
 			{
-
-
-				//CALCULATE SCORE
-
-
+				displayGUI = true;
 			}
 		}
 	}
 
 	void Sound()
 	{
-		if(Random.Range(0, 2) == 0)
+		if(cooldown <= 0)
 		{
-			this.audio.clip = hit1;
-			this.audio.Play();
-		}
-		else
-		{
-			this.audio.clip = hit2;
-			this.audio.Play();
+			cooldown = 1.5f;
+
+			if(Random.Range(0, 2) == 0)
+			{
+				this.audio.clip = hit1;
+				this.audio.Play();
+			}
+			else
+			{
+				this.audio.clip = hit2;
+				this.audio.Play();
+			}
 		}
 	}
 
@@ -82,7 +90,7 @@ public class Scorekeeper : MonoBehaviour
 		        || coll.transform.name == "Planet9")
 		{
 			NumberPlanets++;
-			TotalMass += coll.rigidbody2D.mass;
+			TotalPlanetMass += Random.Range(10000f, 1000000f);
 			Sound();
 			Destroy(coll.gameObject, .1f);
 		}
@@ -91,9 +99,36 @@ public class Scorekeeper : MonoBehaviour
 		        || coll.transform.name == "Asteroid2")
 		{
 			NumberAsteroids++;
-			TotalMass += coll.rigidbody2D.mass;
+			TotalAsteroidMass += Random.Range(1000f, 10000f);
 			Sound();
 			Destroy(coll.gameObject, .1f);
+		}
+	}
+
+	void OnGUI()
+	{
+		if(displayGUI)
+		{
+			GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			GUILayout.BeginVertical();
+			GUILayout.FlexibleSpace();
+			
+			GUILayout.Label("You fed the Sun " + TotalPlanetMass + "kg from " + NumberPlanets + " Planets!");
+			GUILayout.Label("You fed the Sun " + TotalAsteroidMass + "kg from " + NumberAsteroids + " Asteroids!");
+			GUILayout.Label("You were going " + Speed + "m/s when you hit the Sun!");
+
+			if(GUILayout.Button("Retry!"))
+			{
+				Application.LoadLevel("Main");
+			}
+			
+			GUILayout.FlexibleSpace();
+			GUILayout.EndVertical();
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+			GUILayout.EndArea();
 		}
 	}
 }
